358523e64131319750ad1612cbd668793236e6ab
root <root@aa>
2018-06-29 09:32:46 +0530

    Dev:0011 -demo commited by Chanchal Rveviews by xyz

--- src/hibernatenamedqueryexample/UseMe.java
+++ src/hibernatenamedqueryexample/UseMe.java
@@ -4,28 +4,35 @@ package hibernatenamedqueryexample;
 import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class UseMe {
 
     public static void main(String[] args) {
         Configuration cfg=new Configuration();
-        Session session = cfg.configure().buildSessionFactory().openSession();
-    Student s1=new Student();
-   s1.setId(101);
-   s1.setName("raju");
-   s1.setPhone("7458435");
-//        Query query1 = session.getNamedQuery("query1");
-//    query1.setInteger("id", 101);
-//       List list = query1.list();
-//       for (Object object : list) {
-//           Student s=(Student)object;
-//           System.out.println(s.getId());
-//           System.out.println(s.getName());
-//           System.out.println(s.getPhone());
-       session.save(s1);
-       session.beginTransaction().commit();
-  
+        SessionFactory sf = cfg.configure().buildSessionFactory();
+        Session session=sf.openSession();
+        Student s = (Student)session.get(Student.class,101);
+        System.out.println(s.getId());
+        System.out.println(s.getName());
+        System.out.println(s.getPhone());
+        
+        session.close();
+        try{
+            Thread.sleep(6000);
+        }
+    catch(Exception e){
+        e.printStackTrace();
     }
+         Session session1=sf.openSession();
+        Student s1 = (Student)session1.get(Student.class,101);
+        System.out.println(s1.getId());
+        System.out.println(s1.getName());
+        System.out.println(s1.getPhone());
+        
+        
+        
     
+    }    
 }
